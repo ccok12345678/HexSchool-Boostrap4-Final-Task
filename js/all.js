123db5264b3de9811679a7bd17ee6318ada2f139
Karl Yang <ccok12345678@gmail.com>
2021-10-28 15:41:29 +0800

    Update 2021-10-28T07:41:26.124Z

--- js/all.js
+++ js/all.js
@@ -1,2 +1,17 @@
-"use strict";var newFun=function(){};newFun();var newFun02=function(){};newFun02();
+"use strict";
+
+var newFun = function newFun() {
+  var msg = 'hi';
+  console.log("".concat(msg, ", Jhon"));
+};
+
+newFun();
+"use strict";
+
+var newFun02 = function newFun02() {
+  var msg = 'hi';
+  console.log("".concat(msg, ", Yang"));
+};
+
+newFun02();
 //# sourceMappingURL=all.js.map
